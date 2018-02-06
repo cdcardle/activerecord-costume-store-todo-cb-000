7b2526b3b2fda9bbd7a800e46ed5e72286d138fd
Chris Cardle <cdcardle@gmail.com>
2018-02-06 17:05:58 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       c.string :name
       c.string :location
       c.integer :costume_inventory
-      c.integer :employees
-      c.boolean :open_or_closed
+      c.integer :num_of_employees
+      c.boolean :still_in_business
       c.datetime :opening_time
       c.datetime :closing_time
     end
